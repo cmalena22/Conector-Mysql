1f6bfdd89df24d70189d24c0c0c887189ae09d6d
Cinthia Iza <cizac1@est.ups.edu.ec>
2022-01-25 15:12:15 -0500

    WAR'

--- tesisveterinaria.sql
+++ tesisveterinaria.sql
@@ -1,8 +1,4 @@
--- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
---
--- Host: localhost    Database: tesisveterinaria
--- ------------------------------------------------------
--- Server version	8.0.27
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
